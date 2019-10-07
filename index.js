5ba2d97e57a7a425646f0a682aac8b431ca3d91f
oanacodes <oana.carlescu@gmail.com>
2019-10-07 15:02:14 +0200

    initialized server, configured environment and created local Database

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 4030
+
+app.listen(port, () => console.log(`I'm listening on ${port}`))
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
